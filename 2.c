7ad17929a29950d2b10d806f4c86563f8a15f89c
pas97363 <62828274+pas97363@users.noreply.github.com>
2021-03-31 07:32:18 +0530

    Add files via upload

--- 2.c
+++ 2.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
-char pt[50],key[50];
-int m[5][5],p[50],q[50],temp[50],a,c[50],i,j,k,flag,alph[26],bet[26],r1,r2,c1,c2;
+char pt[500],key[50],ct[500];
+int m[5][5],p[50],q[500],temp[50],a,c[500],b[500],i,j,k,flag,alph[26],bet[26],r1,r2,c1,c2;
 void main(){
 printf("Enter the Plaintext: \n");
 gets(pt);
@@ -12,10 +12,15 @@ for(i=0;pt[i]!='\0';i++)
 q[i]=pt[i];
 for(int i=0;key[i]!='\0';i++)
 p[i]=key[i];
+matr();
 encr();
-//decr();
+//printf("Enter the Ciphertext: \n");
+//gets(ct);
+//for(i=0;ct[i]!='\0';i++)
+//c[i]=ct[i];
+decr();
 }
-void encr(){
+void matr(){
 i=0,j=0;
 while(p[i]!='\0'){
 for(k=0;temp[k]!='\0';k++){
@@ -37,8 +42,6 @@ for(k=0;temp[k]!='\0';k++)
 len++;
 for(k=0;k<26;k++)
 alph[k]=k+97;
-/*for(k=0;k<26;k++)
-printf("%c %d\n",alph[k],alph[k]);*/
 i=0,j=0;
 while(alph[i]!='\0'){
 if(alph[i] == 106)
@@ -59,20 +62,10 @@ i++;
 }
 }
 }
-/*for(k=0;bet[k]!='\0';k++)
-printf("%c %d\n",bet[k],bet[k]);*/
-/*for(i=0;temp[i]!='\0';i++)
-printf("%c",temp[i]);*/
-/*for(i=0;i<5 && 5*(i+1)<len;i++){
-for(j=0;j<5 && (5*i)+j<len;j++){
-m[i][j]=temp[(5*i)+j];
-}
-}*/
 for(k=0;temp[k]!='\0';k++)
 m[k/5][k%5]=temp[k];
-for(k=0;bet[k]!='\0';k++){
+for(k=0;bet[k]!='\0';k++)
 m[(len+k)/5][(len+k)%5]=bet[k];
-}
 printf("The playfair matrix: \n");
 for(i=0;i<5;i++){
 for(j=0;j<5;j++){
@@ -80,6 +73,8 @@ printf("%c\t",m[i][j]-32);
 }
 printf("\n");
 }
+}
+void encr(){
 int len1=0;
 for(j=0;q[j]!='\0';j++)
 len1++;
@@ -90,13 +85,17 @@ q[j+1]=q[j];
 q[i+1]=120;
 len1++;
 }
+if(q[i+1] == '\0'){
+q[i+2]='\0';
+q[i+1]=120;
+}
 }
 printf("Corrected Plaintext: \n");
 for(i=0;q[i]!='\0';i++)
 printf("%c",q[i]);
 printf("\n");
 for(k=0;q[k]!='\0';k+=2){
-if(q[k+1]!='\0'){
+//if(q[k+1]!='\0'){
 r1=rowchec(q[k]);
 r2=rowchec(q[k+1]);
 c1=colchec(q[k]);
@@ -121,10 +120,11 @@ else{
 c[k]=m[r1][c2];
 c[k+1]=m[r2][c1];
 }
-}
-else{
+//}
+/*else{
 c[k]=q[k];
-}
+printf("ran else\n");
+}*/
 }
 printf("Ciphertext: \n");
 for(i=0;c[i]!='\0';i++){
@@ -143,5 +143,37 @@ for(i=0;i<5;i++)
 for(j=0;j<5;j++)
 if(m[i][j] == ke)
 return j;
+}
+void decr(){
+for(k=0;c[k]!='\0';k+=2){
+r1=rowchec(c[k]);
+r2=rowchec(c[k+1]);
+c1=colchec(c[k]);
+c2=colchec(c[k+1]);
+if(r1 == r2){
+if(c1 <= 0)
+c1=c1+5;
+b[k]=m[r1][c1-1];
+if(c2 <= 0)
+c2=c2+5;
+b[k+1]=m[r2][c2-1];
+}
+else if(c1 == c2){
+if(r1 <= 0)
+r1=r1+5;
+b[k]=m[r1-1][c1];
+if(r2 <= 0)
+r2=c2+5;
+b[k+1]=m[r2-1][c2];
+}
+else{
+b[k]=m[r1][c2];
+b[k+1]=m[r2][c1];
+}
+}
+printf("Plaintext: \n");
+for(i=0;b[i]!='\0';i++)
+printf("%c",b[i]);
+printf("\n");
 }
 
