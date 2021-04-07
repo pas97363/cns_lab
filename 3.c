1de289f5c8b4c38f6c78a4ba9073d7b0222b21be
pas97363 <62828274+pas97363@users.noreply.github.com>
2021-04-07 13:35:15 +0530

    Add files via upload

--- 3.c
+++ 3.c
@@ -2,36 +2,37 @@
 #include<stdlib.h>
 #include<string.h>
 char pt[50];
-int p[50],key[2][2],c[50],i,j,k,temp,len;
+int p[50],key[10][10],c[50],i,j,k,temp,len,s;
 void main(){
 printf("Enter the Plaintext: \n");
 gets(pt);
 for(i=0;pt[i]!='\0';i++)
-p[i]=(pt[i]-97);
+p[i]=pt[i]%97;
+printf("Enter the size of the key matrix: \n");
+scanf("%d",&s);
 printf("Enter the key matrix: \n");
-for(i=0;i<2;i++)
-for(j=0;j<2;j++)
+for(i=0;i<s;i++)
+for(j=0;j<s;j++)
 scanf("%d",&key[i][j]);
 len=0;
 for(i=0;pt[i]!='\0';i++)
 len++;
-len=len/2;
 encr();
 //decr();
 }
 void encr(){
 k=0;
-while(p[k]!='\0'){
-for(i=0;i<len && (i+k)<len*2;i++){
-for(j=0;j<2;j++){
-c[i+k]=(((p[k]*key[0][i%2])+(p[k+1]*key[1][i%2]))%26)+65;
-//c[i+k+1]=(((p[k]*key[0][i%2+1])+(p[k+1]*key[1][i%2+1]))%26)+65;
-//printf("c[%d]=%d %d %d\n",(i+k),c[i+k]-65,i,k);
-//printf("c[%d]=%d\n",(i+k+1),c[i+k+1]-65);
-
+while(k<len){
+for(j=0;j<s;j++){
+for(i=k;i<(k+s);i++){
+//c[k+j]=(((p[k]*key[k%2][j])+(p[k+1]*key[(k+1)%2][j])));
+c[k+j]+=(p[i]*key[i%s][j]);
+//printf("%d=%d*%d  \n",p[i]*key[i%s][j],p[i],key[i%s][j]);
+//printf("c[%d]=%d %d \n",(k+j),c[k+j],k);
 }
+c[k+j]=(c[k+j]%26)+65;
 }
-k+=2;
+k+=s;
 }
 printf("Ciphertext: \n");
 for(i=0;c[i]!='\0';i++)
