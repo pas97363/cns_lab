d9a339fce6d52bcfb37b5efb28bc8a37643c96c1
pas97363 <62828274+pas97363@users.noreply.github.com>
2021-04-12 14:11:29 +0530

    Add files via upload

--- 3.c
+++ 3.c
@@ -2,7 +2,7 @@
 #include<stdlib.h>
 #include<string.h>
 char pt[50];
-int p[50],key[10][10],c[50],i,j,k,temp,len,s;
+int p[50],key[10][10],c[50],t[50],i,j,k,temp,len,s,inv[26],det;
 void main(){
 printf("Enter the Plaintext: \n");
 gets(pt);
@@ -25,7 +25,7 @@ for(i=0;i<s;i++)
 for(j=0;j<s;j++)
 scanf("%d",&key[i][j]);
 encr();
-//decr();
+decr();
 }
 void encr(){
 k=0;
@@ -44,5 +44,59 @@ k+=s;
 printf("Ciphertext: \n");
 for(i=0;c[i]!='\0';i++)
 printf("%c",c[i]);
+printf("\n");
+}
+void decr(){
+det=0;
+for(i=0;i<len;i++)
+c[i]=c[i]%65;
+int a[s][s],m[s][s];
+for(k=1;k<26;k++)
+for(i=1;i<26;i++)
+for(j=-25;j<26;j++){
+if(((26*i)+(k*j)) == 1)
+inv[k]=j+26;
+//printf("%d %d %d\n",k,i,j+26);
+}
+for(i=0;i<s;i++){
+for(j=0;j<s;j++){
+m[j][i]=(key[(i+1)%s][(j+1)%s]*key[(i+2)%s][(j+2)%s])-(key[(i+1)%s][(j+2)%s]*key[(i+2)%s][(j+1)%s]);
+}
+}
+for(i=0;i<s;i++)
+det+=m[i][0]*key[0][i];
+det=det%26;
+if(det<0)
+det+=26;
+det=inv[det];
+printf("Det inverse: %d\n",det);
+for(i=0;i<s;i++)
+for(j=0;j<s;j++){
+m[i][j]=(det*m[i][j])%26;
+if(m[i][j]<0)
+m[i][j]+=26;
+}
+printf("Inverse key matrix:\n");
+for(i=0;i<s;i++){
+for(j=0;j<s;j++)
+printf("%d\t",m[i][j]);
+printf("\n");
+}
+k=0;
+while(k<len){
+for(j=0;j<s;j++){
+for(i=k;i<(k+s);i++){
+//c[k+j]=(((p[k]*key[k%2][j])+(p[k+1]*key[(k+1)%2][j])));
+t[k+j]+=(c[i]*m[i%s][j]);
+//printf("%d=%d*%d  \n",p[i]*key[i%s][j],p[i],key[i%s][j]);
+//printf("c[%d]=%d %d \n",(k+j),c[k+j],k);
+}
+t[k+j]=(t[k+j]%26)+97;
+}
+k+=s;
+}
+printf("Plaintext: \n");
+for(i=0;t[i]!='\0';i++)
+printf("%c",t[i]);
 printf("\n");
 }
