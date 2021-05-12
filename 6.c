15191f179ab25ee4c5e91ffab8929fa3f6e28beb
pas97363 <62828274+pas97363@users.noreply.github.com>
2021-05-12 15:00:15 +0530

    Add files via upload

--- 6.c
+++ 6.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 int *j;
-int p[10],k[10],k1[8],k2[8],p10[10],p8[8],ip[8],ep[8],ipi[8],p4[4],i,l,m,temp,q[4],s[4],si[4];
+int p[8],c[8],k[10],k1[8],k2[8],p10[10],p8[8],ip[8],ep[8],ipi[8],p4[4],i,l,m,temp,q[4],s[4],si[4];
 int p10t[10]={3,5,2,7,4,10,1,9,8,6},p8t[8]={6,3,7,4,8,5,10,9},ipt[8]={2,6,3,1,4,8,5,7};
 int ept[8]={4,1,2,3,2,3,4,1},p4t[4]={2,4,3,1},ipit[8]={4,1,3,5,7,2,8,6};
 int s0[4][4]={{1,0,3,2},{3,2,1,0},{0,2,1,3},{3,1,3,2}};
@@ -16,6 +16,48 @@ r[i]=1;
 }
 return r;
 }
+void Ep(){
+for(i=0;i<8;i++)
+ep[i]=s[(ept[i]-1)];
+}
+void Fk(){
+for(i=0;i<4;i=i+2){
+si[i]=(s0[(2*(*(j+0+(2*i))))+(*(j+3+(2*i)))][(2*(*(j+1+(2*i))))+(*(j+2+(2*i)))])/2;
+if(i == 2)
+si[i]=(s1[(2*(*(j+0+(2*i))))+(*(j+3+(2*i)))][(2*(*(j+1+(2*i))))+(*(j+2+(2*i)))])/2;
+}
+for(i=1;i<4;i=i+2){
+si[i]=(s0[(2*(*(j+0+(2*(i-1)))))+(*(j+3+(2*(i-1))))][(2*(*(j+1+(2*(i-1)))))+(*(j+2+(2*(i-1))))])%2;
+if(i == 3)
+si[i]=(s1[(2*(*(j+0+(2*(i-1)))))+(*(j+3+(2*(i-1))))][(2*(*(j+1+(2*(i-1)))))+(*(j+2+(2*(i-1))))])%2;
+}
+}
+void P4(){
+for(i=0;i<4;i++)
+p4[i]=si[(p4t[i]-1)];
+j=exor(p4,q);
+}
+void Sip(){
+for(i=0;i<8;i++){
+if(i<4)
+ip[i]=s[i];
+if(i>3)
+ip[i]=*(j+i-4);
+}
+}
+void Ip(){
+for(i=0;i<8;i++){
+if(i>3)
+ip[i]=s[i-4];
+if(i<4)
+ip[i]=*(j+i);
+}
+}
+void Printing(int a[8]){
+for(i=0;i<8;i++)
+printf("%d",a[i]);
+printf("\n");
+}
 void main(){
 printf("Enter the Plaintext: \n");
 for(i=0;i<8;i++)
@@ -26,20 +68,20 @@ scanf("%d",&k[i]);
 k1gen();
 k2gen();
 printf("Key 1: \n");
-for(i=0;i<8;i++)
-printf("%d",k1[i]);
-printf("\nKey 2: \n");
-for(i=0;i<8;i++)
-printf("%d",k2[i]);
-printf("\n");
+Printing(k1);
+printf("Key 2: \n");
+Printing(k2);
 e1();
 e2();
+for(i=0;i<8;i++)
+c[i]=ipi[i];
+d1();
+d2();
 }
 void k1gen(){
 for(i=0;i<10;i++){
 temp=(p10t[i]-1);
 p10[i]=k[temp];
-//printf("%d %d\n",p10[i],temp);
 }
 ls_1();
 for(i=0;i<8;i++){
@@ -71,97 +113,65 @@ s[i-4]=ip[i];
 if(i<4)
 q[i]=ip[i];
 }
-for(i=0;i<8;i++)
-ep[i]=s[(ept[i]-1)];
-/*for(i=0;i<8;i++)
-printf("%d\t",ep[i]);
-printf("\n");*/
+Ep();
 j=exor(ep,k1);
-/*for(i=0;i<8;i++)
-printf("%d\t",*(j+i));
-printf("\n");*/
-for(i=0;i<4;i=i+2){
-si[i]=(s0[(2*(*(j+0+(2*i))))+(*(j+3+(2*i)))][(2*(*(j+1+(2*i))))+(*(j+2+(2*i)))])/2;
-if(i == 2)
-si[i]=(s1[(2*(*(j+0+(2*i))))+(*(j+3+(2*i)))][(2*(*(j+1+(2*i))))+(*(j+2+(2*i)))])/2;
-//printf("%d\t%d\t%d\n",(2*(*(j+0+(2*i))))+(*(j+3+(2*i))),(2*(*(j+1+(2*i))))+(*(j+2+(2*i))),s0[(2*(*(j+0+(2*i))))+(*(j+3+(2*i)))][(2*(*(j+1+(2*i))))+(*(j+2+(2*i)))]);
-}
-for(i=1;i<4;i=i+2){
-si[i]=(s0[(2*(*(j+0+(2*(i-1)))))+(*(j+3+(2*(i-1))))][(2*(*(j+1+(2*(i-1)))))+(*(j+2+(2*(i-1))))])%2;
-if(i == 3)
-si[i]=(s1[(2*(*(j+0+(2*(i-1)))))+(*(j+3+(2*(i-1))))][(2*(*(j+1+(2*(i-1)))))+(*(j+2+(2*(i-1))))])%2;
-}
-/*for(i=0;i<4;i++)
-printf("%d\t",si[i]);
-printf("\n");*/
-for(i=0;i<4;i++)
-p4[i]=si[(p4t[i]-1)];
-/*for(i=0;i<4;i++)
-printf("%d",p4[i]);
-printf("\n");*/
-j=exor(p4,q);
-for(i=0;i<8;i++){
-if(i<4)
-ip[i]=s[i];
-if(i>3)
-ip[i]=*(j+i-4);
-}
+Fk();
+P4();
+Sip();
 printf("Swapped Plaintext after 1 round: \n");
-for(i=0;i<8;i++)
-printf("%d",ip[i]);
-printf("\n");
+Printing(ip);
 }
 void e2(){
 for(i=0;i<8;i++){
-//ip[i]=p[(ipt[i]-1)];
 if(i>3)
 s[i-4]=ip[i];
 if(i<4)
 q[i]=ip[i];
 }
-for(i=0;i<8;i++)
-ep[i]=s[(ept[i]-1)];
-/*for(i=0;i<8;i++)
-printf("%d\t",ep[i]);
-printf("\n");*/
+Ep();
 j=exor(ep,k2);
-/*for(i=0;i<8;i++)
-printf("%d\t",*(j+i));
-printf("\n");*/
-for(i=0;i<4;i=i+2){
-si[i]=(s0[(2*(*(j+0+(2*i))))+(*(j+3+(2*i)))][(2*(*(j+1+(2*i))))+(*(j+2+(2*i)))])/2;
-if(i == 2)
-si[i]=(s1[(2*(*(j+0+(2*i))))+(*(j+3+(2*i)))][(2*(*(j+1+(2*i))))+(*(j+2+(2*i)))])/2;
-//printf("%d\t%d\t%d\n",(2*(*(j+0+(2*i))))+(*(j+3+(2*i))),(2*(*(j+1+(2*i))))+(*(j+2+(2*i))),s0[(2*(*(j+0+(2*i))))+(*(j+3+(2*i)))][(2*(*(j+1+(2*i))))+(*(j+2+(2*i)))]);
-}
-for(i=1;i<4;i=i+2){
-si[i]=(s0[(2*(*(j+0+(2*(i-1)))))+(*(j+3+(2*(i-1))))][(2*(*(j+1+(2*(i-1)))))+(*(j+2+(2*(i-1))))])%2;
-if(i == 3)
-si[i]=(s1[(2*(*(j+0+(2*(i-1)))))+(*(j+3+(2*(i-1))))][(2*(*(j+1+(2*(i-1)))))+(*(j+2+(2*(i-1))))])%2;
-}
-/*for(i=0;i<4;i++)
-printf("%d\t",si[i]);
-printf("\n");*/
-for(i=0;i<4;i++)
-p4[i]=si[(p4t[i]-1)];
-/*for(i=0;i<4;i++)
-printf("%d",p4[i]);
-printf("\n");*/
-j=exor(p4,q);
-for(i=0;i<8;i++){
-if(i>3)
-ip[i]=s[i-4];
-if(i<4)
-ip[i]=*(j+i);
-}
+Fk();
+P4();
+Ip();
 printf("Plaintext after 2 round: \n");
-for(i=0;i<8;i++)
-printf("%d",ip[i]);
-printf("\n");
+Printing(ip);
 for(i=0;i<8;i++)
 ipi[i]=ip[(ipit[i]-1)];
 printf("Ciphertext: \n");
+Printing(ipi);
+}
+void d1(){
+for(i=0;i<8;i++){
+ip[i]=c[(ipt[i]-1)];
+if(i>3)
+s[i-4]=ip[i];
+if(i<4)
+q[i]=ip[i];
+}
+Ep();
+j=exor(ep,k2);
+Fk();
+P4();
+Sip();
+printf("Swapped Ciphertext after 1 round: \n");
+Printing(ip);
+}
+void d2(){
+for(i=0;i<8;i++){
+if(i>3)
+s[i-4]=ip[i];
+if(i<4)
+q[i]=ip[i];
+}
+Ep();
+j=exor(ep,k1);
+Fk();
+P4();
+Ip();
+printf("Ciphertext after 2 round: \n");
+Printing(ip);
 for(i=0;i<8;i++)
-printf("%d",ipi[i]);
-printf("\n");
+ipi[i]=ip[(ipit[i]-1)];
+printf("Plaintext: \n");
+Printing(ipi);
 }
